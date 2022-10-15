83f115cce2fa5d62288731da3ff654630317c0fe
Guillermo Coto <memisguiller202@gmail.com>
2022-10-14 22:50:14 -0600

    Se anaden los SP para validar el usuario o los logins

---
+++ LoginsNuevos.sql
@@ -0,0 +1,27 @@
+USE [TareaProgramada2]
+GO
+CREATE PROCEDURE [dbo].[SP_LoginUsuario]
+	@inUsuario varchar(16),
+	@inPassword varchar(16),
+	@outResult INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON	
+	BEGIN TRY
+	SET @outResult=0;
+		SELECT NombreUsuario FROM [dbo].[Usuario] WHERE NombreUsuario = @inUsuario AND Password = @inPassword AND TipoUsuario = 'Propietario'
+	END TRY
+	 	BEGIN CATCH		
+		/*INSERT INTO [dbo].[DBErrors] VALUES (
+			SUSER_SNAME(),
+			ERROR_NUMBER(),
+			ERROR_STATE(),
+			ERROR_SEVERITY(),
+			ERROR_LINE(),
+			ERROR_PROCEDURE(),
+			ERROR_MESSAGE(),
+			GETDATE());*/
+		Set @outResult=50005;	
+	END CATCH
+	SET NOCOUNT OFF
+END
